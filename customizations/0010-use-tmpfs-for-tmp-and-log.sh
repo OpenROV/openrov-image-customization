944756ea95f406c84cf7f12bc00186e22bf3ef00
BrianAdams <brian@babastik.com>
2015-04-07 00:31:17 -0700

    fixed exit code

--- customizations/0010-use-tmpfs-for-tmp-and-log.sh
+++ customizations/0010-use-tmpfs-for-tmp-and-log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-cat /etc/fstab | grep '/tmp' && exit(0)
+cat /etc/fstab | grep '/tmp' && exit 0
 
 echo "tmpfs   /tmp         tmpfs   nodev,nosuid          0  0" >> /etc/fstab
 echo "tmpfs   /var/log         tmpfs   nodev,nosuid          0  0" >> /etc/fstab
--- customizations/0020-tune-vm-dirty-for-flash-storage.sh
+++ customizations/0020-tune-vm-dirty-for-flash-storage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-cat /etc/sysctl.conf | grep '# Injected by OpenROV_Customize_Image' && exit(0)
+cat /etc/sysctl.conf | grep '# Injected by OpenROV_Customize_Image' && exit 0
 
 echo "# Injected by OpenROV_Customize_Image" >> /etc/sysctl.conf
 echo "vm.dirty_background_ratio = 5" >> /etc/sysctl.conf
