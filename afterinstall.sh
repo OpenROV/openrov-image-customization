2d713b79c50f020382034b67b820ff57a0d4d3c9
badevguru <brian@babastik.com>
2015-04-16 14:21:32 -0700

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -5,6 +5,12 @@ for file in /opt/openrov/image-customization/customizations/*; do
   echo "Executing: $file"
   bash -xe $file
 done
-#flush all buffers to disk
-sync
-blockdev --flushbufs /dev/root
+
+if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
+  echo "We are chrooted!"
+else
+  #flush all buffers to disk
+  sync
+  blockdev --flushbufs /dev/root
+fi
+
