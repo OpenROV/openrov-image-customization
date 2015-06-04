7a5c07134483270c6add3e05e1393bf7470c2b61
BrianAdams <brian@babastik.com>
2015-06-04 10:13:43 -0700

    working around apache error

--- customizations/0040-remove-apache.sh
+++ customizations/0040-remove-apache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -xe
-dpkg --list | grep 'apache2' && apt-get -y remove apache2
+#dpkg --list | grep 'apache2' && apt-get -y remove apache2
+dpkg --list | grep 'apache2' && echo "We should remove apache to make space."
