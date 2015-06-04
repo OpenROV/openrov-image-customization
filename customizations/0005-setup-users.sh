86ac43640e0d988c497c2400aca4d6a64035e957
BrianAdams <brian@babastik.com>
2015-06-04 00:57:34 -0700

    Correcting username com=are

--- customizations/0005-setup-users.sh
+++ customizations/0005-setup-users.sh
@@ -4,8 +4,8 @@ set -xe
 echo Setting up users
 echo -----------------------------
 
-id -u rov
-if [ ($?) -eq 1 ]; then
+
+if ! id -u rov > /dev/null 2>&1; then
   echo Adding user 'rov'
   useradd rov -m -s /bin/bash -g admin
   echo rov:OpenROV | chpasswd
@@ -14,8 +14,7 @@ echo "rov ALL=NOPASSWD: /opt/openrov/cockpit/linux/" >> /etc/sudoers
 echo "rov ALL=NOPASSWD: /opt/openrov/dashboard/linux/" >> /etc/sudoers
 fi
 
-id -u ubuntu
-if [ ($?) -eq 0 ]; then
+if id -u ubuntu > /dev/null 2>&1; then
   userdel -r -f ubuntu
 
   echo -----------------------------
