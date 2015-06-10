4dc81f353c086bc33784cbfd56251b449c06fa72
BrianAdams <brian@babastik.com>
2015-06-09 19:59:21 -0700

    Moving node install yet again

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -5,7 +5,7 @@ set -xe
 #prep for nodejs
 #apt-get install -y curl
 #curl -sL https://deb.nodesource.com/setup | bash -
-wget -qO- https://deb.nodesource.com/setup | bash -
+#wget -qO- https://deb.nodesource.com/setup | bash -
 
 #remove apache2
 #apt-get remove -y apache2
