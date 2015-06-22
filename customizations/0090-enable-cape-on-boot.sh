de058b1f62cda1f1f2a1e4e0bceba40bf0e73f27
BrianAdams <brian@babastik.com>
2015-06-21 19:05:52 -0700

    work around git qemu stability issue

--- customizations/0090-enable-cape-on-boot.sh
+++ customizations/0090-enable-cape-on-boot.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -xe
-pushd /opt
-git clone https://github.com/RobertCNelson/dtb-rebuilder
-cd dtb-rebuilder/
-git checkout origin/3.14-ti
 
+wget https://github.com/RobertCNelson/dtb-rebuilder/tarball/3.19.x
+tar xpvf 3.19.x
+mv RobertCNelson-dtb-rebuilder-* /opt/dtb-rebuilder
+
+pushd /opt/dtb-rebuilder
 sed -i 's|/\* #include "am335x-bone-spi0-spidev\.dtsi" \*/|#include "am335x-bone-spi0-spidev\.dtsi"|g' src/arm/am335x-boneblack.dts
 sed -i 's|/\* #include "am335x-bone-ttyO1\.dtsi" \*/|#include "am335x-bone-ttyO1\.dtsi"|g' src/arm/am335x-boneblack.dts
 make
 sudo make install
+popd
