52b415d2b83d709f7a86ea89c9a0b17fdeb102fb
badevguru <brian@babastik.com>
2015-08-28 14:27:20 -0700

    Handle source images uses hostname beaglebone

--- customizations/0060-change-to-rov-network-names.sh
+++ customizations/0060-change-to-rov-network-names.sh
@@ -7,3 +7,5 @@ echo -----------------------------
 
 set -i 's/arm/OpenROV/g' /etc/hosts
 set -i 's/arm/OpenROV/g' /etc/hostname
+set -i 's/beaglebone/OpenROV/g' /etc/hosts
+set -i 's/beaglebone/OpenROV/g' /etc/hostname
