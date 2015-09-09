c1ebdd2b5633a5ca4d7e4f9ae1162b60018269cc
BrianAdams <brian@babastik.com>
2015-09-08 21:48:43 -0700

    don't run nom post install if no files

--- afterinstall.sh
+++ afterinstall.sh
@@ -19,10 +19,10 @@ npm --version
 popd
 
 
-for file in /opt/openrov/image-customization/post-npmpackages-customizations/*; do
-  echo "Executing: $file"
-  bash -xe $file
-done
+#for file in /opt/openrov/image-customization/post-npmpackages-customizations/*; do
+#  echo "Executing: $file"
+#  bash -xe $file
+#done
 
 
 if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
