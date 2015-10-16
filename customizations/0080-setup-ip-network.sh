9af5f8f813d8b2bb99c92bf9af16c590592b8df0
codewithpassion <dominik.fretz@gmail.com>
2015-10-16 12:43:37 -0700

    Moving eth0:0 config to file in /etc/network/interfaces.d/eth0.cfg

--- customizations/0080-setup-ip-network.sh
+++ customizations/0080-setup-ip-network.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -xe
 
-cat /etc/network/interfaces | grep 'address 192.168.254.1' && exit 0
+cat /etc/network/interfaces.d/* | grep 'address 192.168.254.1' && exit 0
 
 ## fix network
 
-cat >> /etc/network/interfaces << __EOF__
+cat >> /etc/network/interfaces.d/eth0.cfg << __EOF__
 
 auto eth0:0
 iface eth0:0 inet static
