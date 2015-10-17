ee857f123d97ab3bd8361ff1304eafc00d9f1d78
codewithpassion <dominik.fretz@gmail.com>
2015-10-17 11:19:24 -0700

    Moving devicetree overlays to customisation (away from cockpit)

---
+++ customizations/0160-update-devicetree-overlays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -xe
+SCRIPT_PATH="`dirname \"$BASH_SOURCE\"`"
+
+echo "Creating OpenROV device tree overlays"
+dtc -O dtb -o /lib/firmware/OPENROV-RESET-00A0.dtbo -b 0 -@ $SCRIPT_PATH/../device-tree-overlays/OPENROV-RESET-00A0.dts  
+dtc -O dtb -o /lib/firmware/BB-SPI0DEV-00A0.dtbo -b 0 -@ $SCRIPT_PATH/../device-tree-overlays/BB-SPI0DEV-00A0.dts  
+echo "Done creating device tree overlays"
