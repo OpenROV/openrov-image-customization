b831260af6bcd597c4c2ddc5ec76d9950e13bf7b
BrianAdams <brian@babastik.com>
2015-11-04 13:25:04 -0800

    re-enabled nginx

--- customizations/0090-enable-cape-on-boot.sh
+++ customizations/0090-enable-cape-on-boot.sh
@@ -3,15 +3,6 @@
 #temporarily disable as we prep for the latest kernel
 exit 0
 
-set -xe
-wget https://github.com/RobertCNelson/dtb-rebuilder/tarball/3.14-ti
-tar xpvf 3.14-ti
-if [ -d "/opt/dtb-rebuilder" ]; then
-  rm -rf /opt/dtb-rebuilder/
-fi
-
-mv RobertCNelson-dtb-rebuilder-* /opt/dtb-rebuilder
-
 pushd /opt/dtb-rebuilder
 sed -i 's|/\* #include "am335x-bone-spi0-spidev\.dtsi" \*/|#include "am335x-bone-spi0-spidev\.dtsi"|g' src/arm/am335x-boneblack.dts
 sed -i 's|/\* #include "am335x-bone-ttyO1\.dtsi" \*/|#include "am335x-bone-ttyO1\.dtsi"|g' src/arm/am335x-boneblack.dts
