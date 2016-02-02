08e6a5ef91febf50fbb840c7aacdb8427de2ef22
BrianAdams <brian@babastik.com>
2016-02-02 12:20:03 -0800

    Add dummy interface for local service discovery

---
+++ customizations/0170-add-service-discovery-interface.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -xe
+
+cat /etc/network/interfaces.d/* | grep 'address dummy' && exit 0
+
+## fix network
+
+cat >> /etc/network/interfaces.d/dummy0.cfg << __EOF__
+
+auto dummy0
+iface dummy0 inet static
+address 10.0.0.1
+netmask 255.255.255.0
+multicast
+
+__EOF__
