42d4c8236deda7cae58ed0315e2831d4f1162711
BrianAdams <brian@babastik.com>
2016-02-08 15:57:26 -0800

    force dummy module to load on start

--- customizations/0170-add-service-discovery-interface.sh
+++ customizations/0170-add-service-discovery-interface.sh
@@ -5,6 +5,8 @@ cat /etc/network/interfaces.d/* | grep 'address dummy' && exit 0
 
 ## fix network
 
+echo "dummy" >> /etc/modules-load.d/modules.conf
+
 cat >> /etc/network/interfaces.d/dummy0.cfg << __EOF__
 
 auto dummy0
