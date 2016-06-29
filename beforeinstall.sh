26e3825e4b28fbdf62c02e4b64549877613abb55
Charles Cross <charles@openrov.com>
2016-06-29 11:24:06 -0700

    Removed arduino fix step, removed dtb steps, updated version number

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
 set -xe
-
-
-#prep for nodejs
-#apt-get install -y curl
-#curl -sL https://deb.nodesource.com/setup | bash -
-#wget -qO- https://deb.nodesource.com/setup | bash -
-
-#remove apache2
-#apt-get remove -y apache2
