761bc51c72bf433f4549833b3b6511d9cd8f0f66
Charles Cross <charles@openrov.com>
2016-07-06 14:24:31 -0700

    Cloud9 steps temporarily commented out due to conflict with node6

--- customizations/0130-customize-cloud9.sh
+++ customizations/0130-customize-cloud9.sh
@@ -2,8 +2,8 @@
 set -xe
 
 #sed -i 's/port: process.env.PORT || 3000/port: process.env.PORT || 3131/g' /opt/cloud9/build/standalonebuild/settings/standalone.js
-cp /lib/systemd/system/cloud9.service /etc/systemd/system
-cp /lib/systemd/system/cloud9.socket /etc/systemd/system
-sed -i 's|/var/lib/cloud9|/opt/openrov|g' /etc/systemd/system/cloud9.service
-sed -i 's|3000|3131|g' /etc/systemd/system/cloud9.socket
-systemctl daemon-reload
+# cp /lib/systemd/system/cloud9.service /etc/systemd/system
+# cp /lib/systemd/system/cloud9.socket /etc/systemd/system
+# sed -i 's|/var/lib/cloud9|/opt/openrov|g' /etc/systemd/system/cloud9.service
+# sed -i 's|3000|3131|g' /etc/systemd/system/cloud9.socket
+# systemctl daemon-reload
